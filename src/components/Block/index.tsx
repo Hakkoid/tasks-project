61daa732cf28f85bb722a6ee1e53196376c56012
Alexander <hakkoid@MacBook-Pro-Alexander.local>
2020-09-26 21:19:31 +0300

    Added the@bem-react/classname to Block component

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -1,16 +1,19 @@
 import { Component, Vue } from 'vue-property-decorator'
+import { cn } from '@bem-react/classname'
 
 import './style.scss'
+
+const bemCls = cn('Block')
 
 @Component
 export default class Block extends Vue {
   render() {
     return (
-      <div class='Block'>
-        <h2 class='Block-Title'>
+      <div class={bemCls()}>
+        <h2 class={bemCls('Title')}>
           {this.$slots.title}
         </h2>
-        <div class='Block-Content'>
+        <div class={bemCls('Content')}>
           {this.$slots.default}
         </div>
       </div>
